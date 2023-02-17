4f7a1c5b068f89c5b0488b1ea3c174c4ffa8dba2
shanart <shanin.x@gmail.com>
2023-02-17 07:53:58 +0200

    auth service started

---
+++ authentication-service/cmd/api/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"authentication/cmd/api/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+
+	log.Println("Starting authentication service")
+
+	app := Config{}
+
+	// define http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
